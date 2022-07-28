42242afeb731a594dd539e3b825f8252366199fc
sreejithtsnair <sreejithtsnair@gmail.com>
2022-07-29 01:10:08 +0530

    test

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -1,39 +1,23 @@
 import json
 import unittest
-from app import app
+from project import app
+from project.persistence.users_dao import *
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
 class TestApp(unittest.TestCase):
     token=''
 
+     def test_1_getToken(self):         
+        self.assertTrue(True)
+
     def test_1_get_none(self):
         tester = app.test_client(self)
-        response = tester.get('/api/v1/customers/', content_type='application/json')
+        response = tester.get('/api/v1/products/', content_type='application/json')
         data=json.loads(response.text)
         print(f"get 1: {data}")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data, [])
-
-    def test_2_post(self):
-        tester = app.test_client(self)
-        test_data = {"address": "5 Spenser Plaza",
-                     "email": "kbraksper0@mac.com",
-                     "name": "Kelbee"}
-        response = tester.post('/api/v1/customers/',content_type='application/json', json=test_data)
-        data=json.loads(response.text)
-        print(f"post: {data}")
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(data['id'] != None)
-
-
-    def test_3_get_none(self):
-        tester = app.test_client(self)
-        response = tester.get('/api/v1/customers/', content_type='application/json')
-        data=json.loads(response.text)
-        print(f"get 2: {data}")
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(len(data)>0)
-        
 
     def test_4_testToken(self):
         tester = app.test_client(self)
