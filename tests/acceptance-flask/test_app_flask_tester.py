cd1b9e66e3414fe41ede4fcdcc875603a2adaa21
sreejithtsnair <sreejithtsnair@gmail.com>
2022-07-28 23:38:14 +0530

    name changed

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -1,10 +1,56 @@
 import json
 import unittest
-from project import app 
+from app import app
+
 
 class TestApp(unittest.TestCase):
-
     token=''
-   
-    def test_1_getToken(self):         
-        self.assertTrue(True)
+
+    def test_1_get_none(self):
+        tester = app.test_client(self)
+        response = tester.get('/api/v1/customers/', content_type='application/json')
+        data=json.loads(response.text)
+        print(f"get 1: {data}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data, [])
+
+    def test_2_post(self):
+        tester = app.test_client(self)
+        test_data = {"address": "5 Spenser Plaza",
+                     "email": "kbraksper0@mac.com",
+                     "name": "Kelbee"}
+        response = tester.post('/api/v1/customers/',content_type='application/json', json=test_data)
+        data=json.loads(response.text)
+        print(f"post: {data}")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(data['id'] != None)
+
+
+    def test_3_get_none(self):
+        tester = app.test_client(self)
+        response = tester.get('/api/v1/customers/', content_type='application/json')
+        data=json.loads(response.text)
+        print(f"get 2: {data}")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(data)>0)
+        
+
+    def test_4_testToken(self):
+        tester = app.test_client(self)
+        test_data = {"email":"user@test.com","password":"passwordjd"}
+        response = tester.post('/api/v1/token',content_type='application/json', json=test_data)        
+        data=json.loads(response.text)
+        print(f"post token: {data}")
+        self.assertEqual(response.status_code, 200)
+        if response.status_code==200:
+            TestApp.token=data['token']
+        
+    def test_5_get_restricted(self):
+        tester = app.test_client(self)
+        print(f"token: {self.token}")
+        headers = {"Authorization": f"Bearer {TestApp.token}"}
+
+        response = tester.get('/api/v1/restricted', content_type='application/json', headers=headers)
+        data=json.loads(response.text)
+        print(f"get restricted: {data}")
+        self.assertEqual(response.status_code, 200)
--- tests/acceptance-request/test_app_request.py
+++ tests/acceptance-request/test_app_request.py
@@ -6,5 +6,28 @@ class TestApp(unittest.TestCase):
     token=''
     base_url='http://localhost:5000/api/v1'
 
-    def test_1_getToken(self):
-        self.assertTrue(True)
+    def test_1_get_none(self):
+        url=f"{self.base_url}/customers/"
+        response = requests.get(url)
+        data=json.loads(response.text)
+        print(f"get 1: {data}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data, [])
+
+    def test_2_post(self):
+        url=f"{self.base_url}/customers/"
+        test_data = {"address": "5 Spenser Plaza",
+                     "email": "kbraksper0@mac.com",
+                     "name": "Kelbee"}
+        response = requests.post(url, json=test_data)
+        data=json.loads(response.text)
+        print(f"post: {data}")
+        self.assertEqual(response.status_code, 200)
+
+    def test_3_get_none(self):
+        url=f"{self.base_url}/customers/"
+        response = requests.get(url)
+        data=json.loads(response.text)
+        print(f"get 2: {data}")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(data)>0)
