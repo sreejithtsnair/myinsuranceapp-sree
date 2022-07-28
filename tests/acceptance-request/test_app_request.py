763b7cbc7a5b10a67d517e2097040b1608f05c54
sreejithtsnair <sreejithtsnair@gmail.com>
2022-07-29 02:10:56 +0530

    test

--- tests/acceptance-request/test_app_request.py
+++ tests/acceptance-request/test_app_request.py
@@ -7,7 +7,7 @@ class TestApp(unittest.TestCase):
     base_url='http://localhost:5000/api/v1'
 
     def test_1_get_none(self):
-        url=f"{self.base_url}/customers/"
+        url=f"{self.base_url}/products/"
         response = requests.get(url)
         data=json.loads(response.text)
         print(f"get 1: {data}")
@@ -15,17 +15,17 @@ class TestApp(unittest.TestCase):
         self.assertEqual(data, [])
 
     def test_2_post(self):
-        url=f"{self.base_url}/customers/"
-        test_data = {"address": "5 Spenser Plaza",
-                     "email": "kbraksper0@mac.com",
-                     "name": "Kelbee"}
+        url=f"{self.base_url}/users/"
+        test_data = {"fullname": "sree"
+                     "address": "trivandrum",
+                     "email": "sree@s.com"}
         response = requests.post(url, json=test_data)
         data=json.loads(response.text)
         print(f"post: {data}")
         self.assertEqual(response.status_code, 200)
 
     def test_3_get_none(self):
-        url=f"{self.base_url}/customers/"
+        url=f"{self.base_url}/products/"
         response = requests.get(url)
         data=json.loads(response.text)
         print(f"get 2: {data}")
