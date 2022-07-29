a8febb776c46157c0ab191ab6e62044fcee7a4c7
sreejithtsnair <sreejithtsnair@gmail.com>
2022-07-29 12:26:20 +0530

    test cases

--- tests/acceptance-request/test_app_request.py
+++ tests/acceptance-request/test_app_request.py
@@ -13,3 +13,10 @@ class TestApp(unittest.TestCase):
     def test_2_get_product (self):
         url=f"{self.base_url}/products/2"
         self.assertTrue(True)
+
+    def test_3_invalid_Token(self):
+        url=f"{self.base_url}/products/2"
+        invalid_fake_token='CfDJ8OW5OI0CPGJBgSNlGwO0x4YF7qbYKVv7KOO-N0eFtDUzXOrL7F9Xd9W1otVi4ueJOkAmAhuoHFWNkqRaFD7zvAMHMSKncl6Vo5QXKmpvy6vqxOKxSURdIey8aZPRi3Nnhp2p9la-Al5xrVKz0lignRdcCHf3O7pF9zv_sNx_c_T7pUe3WsxaJEPX3t_9FO2Wjw'
+        headers = {"Authorization": f"Bearer {invalid_fake_token}"}
+        response = requests.get(url, headers=headers)
+        self.assertTrue(response.status_code > 400)
