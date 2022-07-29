706b5c5d7407504efeb39c8d4c439a4ce8dc5b4b
sreejithtsnair <sreejithtsnair@gmail.com>
2022-07-29 12:55:35 +0530

    test cases

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -1,18 +1,11 @@
 import json
-from tkinter import TRUE
-from tokenize import Token
 import unittest
 import requests
-
-from project.controllers.api.user_controller import api_get_user
 
 class TestApp(unittest.TestCase):
     token=''
     base_url='http://localhost:5000/api/v1'
 
-    def test_5_get_restricted(self):
-        tester = Token.test_client(self)
-        print(f"token: {self.token}")
-        headers = {"Authorization": f"Bearer {TestApp.token}"}
-        self.assertTrue(TRUE)
+    def test_1_getToken(self):
+        self.assertTrue(True)
        
