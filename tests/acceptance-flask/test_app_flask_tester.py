80ec30d0543adeae1a7a81884e66e9dd55f91905
sreejithtsnair <sreejithtsnair@gmail.com>
2022-07-29 13:20:37 +0530

    test cases

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -5,8 +5,19 @@ from project import app
 class TestApp(unittest.TestCase):
 
     token=''
-       
-    def test_1_get_user_products_valid_token(self):
+   
+    def test_1_getToken(self):         
+        self.assertTrue(True)
+        tester = app.test_client(self)
+        user_data = {"email":"jd@myinsuranceapp.com","password":"passwordjd"}
+        response = tester.post('/api/v1/token', content_type='application/json', json = user_data)
+        data=json.loads(response.text)
+        print(f"post token: {data}")
+        self.assertEqual(response.status_code, 200)
+        if response.status_code==200:
+            TestApp.token=data['token']
+            
+    def test_2_get_user_products_valid_token(self):
         tester = app.test_client(self)
         print(f"token: {self.token}")
         headers = {"Authorization": f"Bearer {TestApp.token}"}
@@ -16,22 +27,22 @@ class TestApp(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(len(data)>0)
 
-    def test_2_get_user_products_invalid_token(self):
+    def test_3_get_user_products_invalid_token(self):
         tester = app.test_client(self)
-        invalid_fake_token='DJ8OW5OI0CPGJBgSNlGwO0x4YF7qbYKVv7KOO-N0eFtDUzXOrL7F9Xd'
+        invalid_fake_token='CfDJ8OW5OI0CPGJBgSNlGwO0x4YF7qbYKVv7KOO-N0eFtDUzXOrL7F9Xd'
         headers = {"Authorization": f"Bearer {invalid_fake_token}"}
         response = tester.get('/api/v1/users/1/products', content_type='application/json', headers=headers)
         data=json.loads(response.text)
         print(f"get_user_products: {data}")
         self.assertTrue(response.status_code > 400)
 
-    def test_3_get_Token(self):         
+
+    def test_4_invalid_user(self):
         tester = app.test_client(self)
-        user_data = {"email":"jd@myinsuranceapp.com","password":"passwordjd"}
-        response = tester.post('/api/v1/token', content_type='application/json', json = user_data)
+        print(f"token: {self.token}")
+        headers = {"Authorization": f"Bearer {TestApp.token}"}
+        response = tester.get('/api/v1/users/89/products', content_type='application/json', headers=headers)
         data=json.loads(response.text)
-        print(f"post token: {data}")
         self.assertEqual(response.status_code, 200)
-        if response.status_code==200:
-            TestApp.token=data['token']
+
       
