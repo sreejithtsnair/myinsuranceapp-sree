24b6bf4c94fedbff3eac9791fe88e305738bf0e4
sreejithtsnair <sreejithtsnair@gmail.com>
2022-07-29 12:41:25 +0530

    test cases

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -2,10 +2,16 @@ import json
 import unittest
 import requests
 
+from project.controllers.api.user_controller import api_get_user
+
 class TestApp(unittest.TestCase):
     token=''
     base_url='http://localhost:5000/api/v1'
 
-    def test_1_getToken(self):
-        self.assertTrue(True)
+    def test_1_get_token(self):
+        tester = api_get_user.test_client(self)
+        test_data = {"email":"jd@myinsuranceapp.com","password":"passwordjd"}
+        response = tester.post('/api/v1/token',content_type='application/json', json=test_data)
+        data=json.loads(response.text)
+        self.assertEqual(response.status_code, 200)
        
