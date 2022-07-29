d4c6176ee39c14f43ebb8c12f486132638b8c4f6
sreejithtsnair <sreejithtsnair@gmail.com>
2022-07-29 11:58:06 +0530

    test cases

--- tests/acceptance-request/test_app_request.py
+++ tests/acceptance-request/test_app_request.py
@@ -4,30 +4,34 @@ import requests
 
 class TestApp(unittest.TestCase):
     token=''
-    base_url='http://localhost:5000/api/v1'
+    base_url='http://localhost:5000/api/v1/'
 
-    def test_1_get_none(self):
-        url=f"{self.base_url}/products/"
-        response = requests.get(url)
-        data=json.loads(response.text)
-        print(f"get 1: {data}")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(data, [])
-
-    def test_2_post(self):
-        url=f"{self.base_url}/users/"
-        test_data = {"fullname": "sree"
-                     "address": "trivandrum",
-                     "email": "sree@s.com"}
+    def test_1_get_Token(self):
+        url=f"{self.base_url}/token"
+        self.assertTrue(True)
+        test_data = {"email":"jd@myinsuranceapp.com","password":"passwordjd"}
         response = requests.post(url, json=test_data)
         data=json.loads(response.text)
         print(f"post: {data}")
         self.assertEqual(response.status_code, 200)
+        if response.status_code==200:
+            TestApp.token=data['token']
 
-    def test_3_get_none(self):
-        url=f"{self.base_url}/products/"
-        response = requests.get(url)
+    def test_2_get_user_products_valid_token(self):
+        url=f"{self.base_url}/users/2/products"
+        print(f"token: {self.token}")
+        headers = {"Authorization": f"Bearer {TestApp.token}"}
+        response = requests.get(url, headers=headers)
         data=json.loads(response.text)
-        print(f"get 2: {data}")
+        print(f"get_user_products: {data}")
         self.assertEqual(response.status_code, 200)
         self.assertTrue(len(data)>0)
+
+    def test_3_get_user_products_invalid_token(self):
+        url=f"{self.base_url}/users/1/products"
+        invalid_fake_token='CfDJ8OW5OI0CPGJBgSNlGwO0x4YF7qbYKVv7KOO-N0eFtDUzXOrL7F9Xd9W1otVi4ueJOkAmAhuoHFWNkqRaFD7zvAMHMSKncl6Vo5QXKmpvy6vqxOKxSURdIey8aZPRi3Nnhp2p9la-Al5xrVKz0lignRdcCHf3O7pF9zv_sNx_c_T7pUe3WsxaJEPX3t_9FO2Wjw'
+        headers = {"Authorization": f"Bearer {invalid_fake_token}"}
+        response = requests.get(url, headers=headers)
+        data=json.loads(response.text)
+        print(f"get_user_products: {data}")
+        self.assertTrue(response.status_code > 400)
